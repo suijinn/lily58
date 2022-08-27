0706b2c178a9099f18061a2f63362042e5bf9298
Hiroki <h.nakshima1993@gmail.com>
2022-08-27 23:36:57 +0900

    [update] 4層目を追加

--- keymaps/mykeymap/keymap.c
+++ keymaps/mykeymap/keymap.c
@@ -24,6 +24,7 @@ enum layer_number {
   _LOWER,
   _RAISE,
   _ADJUST,
+  _UTIL,
 };
 
 #define RAISE MO(_RAISE)
@@ -51,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
   KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_RBRC,  KC_NUHS,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_INT1,
-                    KC_LGUI, KC_LALT, LT(_LOWER, KC_LNG2), KC_SPC,  KC_ENT,   RSFT_T(KC_LNG1),  MO(_RAISE),   LT(_ADJUST, KC_GRV)
+                    KC_LGUI, KC_LALT, LT(_LOWER, KC_LNG2), KC_SPC,  KC_ENT,   RSFT_T(KC_LNG1),  MO(_RAISE),   LT(_UTIL, KC_GRV)
 ),
 
 // [_QWERTY] = LAYOUT(
@@ -137,7 +138,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
-  [_ADJUST] = LAYOUT(
+[_ADJUST] = LAYOUT(
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, KC_WH_U, XXXXXXX, XXXXXXX,                   KC_BTN5, KC_BTN1, KC_MS_U, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, KC_WH_L, KC_WH_D, KC_WH_R, XXXXXXX,                   KC_BTN4, KC_MS_L, KC_MS_D, KC_MS_R, XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, KC_ACL0, KC_ACL1, KC_ACL2, XXXXXXX, XXXXXXX, XXXXXXX, KC_BTN3, XXXXXXX, KC_WH_U, KC_WH_D, KC_VOLD, KC_VOLU,
+                             _______, _______, _______, _______, _______,  _______, _______, _______
+  ),
+
+[_UTIL] = LAYOUT(
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   XXXXXXX, XXXXXXX, XXXXXXX, KC_WH_U, XXXXXXX, XXXXXXX,                   KC_BTN5, KC_BTN1, KC_MS_U, XXXXXXX, XXXXXXX, XXXXXXX,
   XXXXXXX, XXXXXXX, KC_WH_L, KC_WH_D, KC_WH_R, XXXXXXX,                   KC_BTN4, KC_MS_L, KC_MS_D, KC_MS_R, XXXXXXX, XXXXXXX,
@@ -230,6 +239,8 @@ bool oled_task_user(void) {
     case _ADJUST:
         oled_write_ln_P(PSTR("Adjust"), false);
         break;
+    case _UTIL:
+        oled_write_ln_P(PSTR("Util"), false);
     default:
         oled_write_ln_P(PSTR("Undefined"), false);
     }
